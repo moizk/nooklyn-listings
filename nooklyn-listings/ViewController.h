78d6f667f1b57d3b6e82fc8ca0b448de95646a1e
Moiz K. Malik <moiz@heuretix.com>
2014-12-15 23:23:23 -0500

    Initial Commit

---
+++ nooklyn-listings/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  nooklyn-listings
+//
+//  Created by Moiz K. Malik on 12/15/14.
+//  Copyright (c) 2014 Moiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ nooklyn-listings/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  nooklyn-listings
+//
+//  Created by Moiz K. Malik on 12/15/14.
+//  Copyright (c) 2014 Moiz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
